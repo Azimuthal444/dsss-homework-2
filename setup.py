d87c343263e13eb481a02adace117d7c19e13402
Vignesh V Pai <vigneshvpai@gmail.com>
2023-11-10 21:56:59 +0100

    Added pip packaging setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup File necessary for Python Packaging"""
+from setuptools import find_packages, setup
+
+setup(
+    name="math_quiz",
+    version="1.0",
+    author="Vignesh Vivek Pai (yk88iban)",
+    author_email="my_fau_email@fau.de",
+    packages=find_packages(),
+)
